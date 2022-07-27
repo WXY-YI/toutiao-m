ab5d4ef725a3167fcbf485c559f21bb5b2dec4b5
王夏养 <252780483@qq.com>
2022-07-27 16:57:16 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+/*
+ * @Description:
+ * @Version: 2.0
+ * @Autor: Wang
+ * @Date: 2022-07-27 15:33:16
+ * @LastEditors: Wang
+ * @LastEditTime: 2022-07-27 16:54:49
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,17 @@
+/*
+ * @Description:
+ * @Version: 2.0
+ * @Autor: Wang
+ * @Date: 2022-07-27 15:33:16
+ * @LastEditors: Wang
+ * @LastEditTime: 2022-07-27 16:26:51
+ */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+const routes = []
 
 const router = new VueRouter({
   routes
